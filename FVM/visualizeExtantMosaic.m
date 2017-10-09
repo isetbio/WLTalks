252e6943201ca643611d9d92017d60c270ec7844
Nicolas Cottaris <cottaris@sas.upenn.edu>
2017-10-09 13:25:03 -0400

    Better visualization of labeled cones mosaic.
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- FVM/visualizeExtantMosaic.m
+++ FVM/visualizeExtantMosaic.m
@@ -11,14 +11,17 @@ XYTicks =  150*[-1 -0.5 0 0.5 1]*1e-6;
 XYTickLabels = sprintf('%2.0f\n', XYTicks*1e6);
 XYLims = [XYTicks(1) XYTicks(end)];
 
-% Visualize the inner segment, the outer-segment, or both?
-visualizedAperture = 'lightCollectingArea';                             % choose between 'both', 'lightCollectingArea', 'geometricArea'
+% Export the figure ?
+exportAsPDF = false;
 
 hFig = figure(); clf;
 set(hFig, 'Position', [10 10 2048 1024], 'Color', [1 1 1]);
 
-% 
+% Show the mesh and and the density contours but do not label the cones
 ax = subplot('Position', [0.03 0.05 0.45 0.94]);
+% Visualize the inner segment
+visualizedAperture = 'lightCollectingArea';
+
 theHexMosaic.visualizeGrid(...
     'axes', ax, ...
     'visualizedConeAperture', visualizedAperture, ...
@@ -36,8 +39,11 @@ set(ax, 'XLim', XYLims, 'YLim', XYLims, ...
          'XTickLabel', XYTickLabels,'YTickLabel', XYTickLabels, ...
          'FontSize', 18, 'LineWidth', 1.5);
 
-
+% Label the cones, plot on dark background, show outer-segment for better visibility
 ax = subplot('Position', [0.52 0.05 0.45 0.94]);
+% Visualize the the outer-segment
+visualizedAperture = 'geometricArea';
+backgroundColor = [0.8 0.8 0.8];
 theHexMosaic.visualizeGrid(...
     'axes', ax, ...
     'visualizedConeAperture', visualizedAperture, ...
@@ -50,10 +56,13 @@ box on
 set(ax, 'XLim', XYLims, 'YLim', XYLims, ...
          'XTick', XYTicks, 'YTick', XYTicks, ...
          'XTickLabel', XYTickLabels,'YTickLabel', XYTickLabels, ...
-         'FontSize', 18, 'LineWidth', 1.5);
+         'FontSize', 18, 'LineWidth', 1.5, ...
+         'Color', backgroundColor);
 
+if (exportAsPDF)
 NicePlot.exportFigToPDF('mosaic.pdf', hFig, 300);
-    
+end
+
 end
 
 
